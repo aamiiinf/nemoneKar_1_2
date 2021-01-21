992b85ea1682ab2a51bc4de247ac4c90a8c59459
aamiiin <aamiiinf@gmail.com>
2021-01-20 22:28:12 -0800

    updata mega menu

--- src/app/components/main-top/main-top.component.ts
+++ src/app/components/main-top/main-top.component.ts
@@ -6,5 +6,23 @@ import { Component} from '@angular/core';
   styleUrls: ['./main-top.component.scss']
 })
 export class MainTopComponent{
+  public isTrue;
+  constructor() { }
 
+  ngOnInit(): void {
+    window.addEventListener('scroll', this.scroll, true);
+  }
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+}
+  scroll = (event): void => {
+    const n = event.srcElement.scrollingElement.scrollTop;
+    if(n >= 80){
+      this.isTrue = true;
+      console.log(n);
+    }else if(n < 120){
+      this.isTrue = false;
+      console.log(n);
+    }
+  }
 }
